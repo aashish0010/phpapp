787cec2ccca962cdb0d6fa01081405705247c1f7
coadcleaner <40197016+coadchanger@users.noreply.github.com>
2019-07-18 11:11:36 +0545

    first commit

---
+++ create_program.php
@@ -0,0 +1,57 @@
+<?php
+
+
+$offset= 0;
+
+if(isset($_POST["text"])&& isset($_POST['searchfor'])&& isset($_POST['replacewith'])){
+
+	$text=$_POST['text'];
+	$search=$_POST['searchfor'];
+	$replace=$_POST['replacewith'];
+
+	$search_length= strlen($search);
+
+	if (!empty($text) && !empty($search) && !empty($replace) ) {
+		
+		while ($strpos = strpos($text, $search,$offset)) 
+		{
+
+		$offset = $strpos + $search_length;
+
+		$text= substr_replace($text, $replace, $strpos,$search_length);
+
+		echo $text;
+			
+		}
+
+	}
+	else
+	{
+
+		echo "Please fill all field";
+
+
+	}
+
+
+	}
+
+
+?>
+
+
+
+
+<form method="POST">
+
+	<textarea name="text" rows='6'cols='30'></textarea><br><br>
+
+	Search For:<br>
+	<input type="text" name="searchfor"><br><br>
+
+	Replace With:<br>
+	<input type="text" name="replacewith"><br><br>
+
+	<input type="submit" value="Find and Replace" name="submit">
+
+</form>
